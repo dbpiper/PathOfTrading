86d8b862deb08e8314c826f198fd11727d619462
David Piper <dbpiper@gmail.com>
2018-05-11 15:29:01 -0500

    Finished switch transitions and animations.

--- Site/path_of_trading/src/components/Switch/Switch.js
+++ Site/path_of_trading/src/components/Switch/Switch.js
@@ -24,35 +24,55 @@ class Switch extends React.Component {
     this.transition2to1 = this.transition2to1.bind(this);
     this.transition1to0 = this.transition1to0.bind(this);
     this.transition0to2 = this.transition0to2.bind(this);
+
+    this.transition = this.transition.bind(this);
   }
 
-  transition2to1() {
+
+  transition(animationClassName, finishFunc, animationTime) {
+    const oldClassName = this.buttonEle.className;
+
+    this.buttonEle.className = classNames(
+      oldClassName,
+      animationClassName
+    );
+
+    setTimeout(function() {
+      this.buttonEle.className = oldClassName;
+
+      finishFunc();
+    }.bind(this), animationTime - 50); // account for logic happening
+                                        // -- still racey, but no really good way around
   }
 
-  transition1to0() {
-
+  transition2to1(finishFunc) {
+    this.transition('two-to-one-animation', finishFunc, 166.667);
   }
 
-  transition0to2() {
+  transition1to0(finishFunc) {
+    this.transition('one-to-zero-animation', finishFunc, 333.333);
+    // this.transition('', finishFunc, 333.333);
+  }
 
+  transition0to2(finishFunc) {
+    this.transition('zero-to-two-animation', finishFunc, 166.667);
   }
 
   handleClick() {
 
-    // switch(this.props.value) {
-    //   case 2:
-    //     this.transition2to1();
-    //     break;
-    //   case 1:
-    //     this.transition1to0();
-    //     break;
-    //   case 0:
-    //     this.transition0to2();
-    //     break;
-    // }
+    const finishFunc = () => this.props.onClick(newValue(this.props.value));
+    switch(this.props.value) {
+      case 2:
+        this.transition2to1(finishFunc);
+        break;
+      case 1:
+        this.transition1to0(finishFunc);
+        break;
+      case 0:
+        this.transition0to2(finishFunc);
+        break;
+    }
 
-
-    this.props.onClick(newValue(this.props.value));
   }
 
 
@@ -76,17 +96,32 @@ class Switch extends React.Component {
               onClick={() => this.handleClick()}
             >
               <img
-                className="switch switch-background middle"
+                className={classNames({
+                    "switch": true,
+                    "switch-background": true,
+                    "middle": true,
+                    "hidden": this.props.value !== 2,
+                })}
                 src={SwitchMiddle}
                 ref={(middleEle) => this.middleEle = middleEle}
               />
               <img
-                className="switch switch-background hidden"
+                className={classNames({
+                    "switch": true,
+                    "switch-background": true,
+                    "on": true,
+                    "hidden": this.props.value !== 1,
+                })}
                 src={SwitchOn}
                 ref={(onEle) => this.onEle = onEle}
               />
               <img
-                className="switch switch-background hidden"
+                className={classNames({
+                    "switch": true,
+                    "switch-background": true,
+                    "off": true,
+                    "hidden": this.props.value !== 0,
+                })}
                 src={SwitchOff}
                 ref={(offEle) => this.offEle = offEle}
               />
@@ -111,10 +146,14 @@ class Switch extends React.Component {
 
 function newValue(value) {
   if (value >= 0 && value <= 2) {
-    alert(value);
-    return (value + 1) % 3;
+    if (value === 2) {
+      return 1;
+    } else if (value === 1) {
+      return 0;
+    } else {
+      return 2;
+    }
   } else {
-    alert(value);
     alert('error: switch value outside valid range');
   }
 }
