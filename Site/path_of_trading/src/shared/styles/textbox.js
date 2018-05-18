6081dbaea3eb5eccd3db4613ed4ceec77b46b8f1
David Piper <dbpiper@gmail.com>
2018-05-17 23:16:52 -0500

    Change grey styling of placeholder text in Firefox to standard white color. Also add support for this technique to include old version of Firefox ([4,19), [19, 51)).

--- Site/path_of_trading/src/shared/styles/textbox.js
+++ Site/path_of_trading/src/shared/styles/textbox.js
@@ -12,16 +12,37 @@ const textbox = css`
   /* Chrome, Firefox, Opera, Safari 10.1+ */
   &::placeholder {
     ${standardFont}
+    opacity: 1;
+  }
+
+  /* Chrome < 57, Opera < 44, Safari < 10.1 */
+  &::-webkit-input-placeholder {
+    ${standardFont}
+    opacity: 1;
   }
 
   /* Microsoft Edge */
   &::-ms-input-placeholder {
     ${standardFont}
+    opacity: 1;
   }
 
   /* Internet Explorer 10-11 */
   &:-ms-input-placeholder {
     ${standardFont}
+    opacity: 1;
+  }
+
+  /* 19 <= Firefox < 51 */
+  &::-moz-placeholder {
+    ${standardFont}
+    opacity: 1;
+  }
+
+  /* 2 <= Firefox < 19 */
+  &:-moz-placeholder {
+    ${standardFont}
+    opacity: 1;
   }
 
   &:focus {
